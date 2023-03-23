9b279c4789ee4f63b0aca7b7a022fd9278e7ac4c
Tejomay128 <22d0367@iitb.ac.in>
2023-03-23 09:47:02 -0700

    futher changes to prep_data

--- prep_data.py
+++ prep_data.py
@@ -1,24 +1,27 @@
 from datasets import load_dataset
 import torch
-from transformers import MT5TokenizerFast
+from transformers import MT5Tokenizer
 
 
-def sort_input_by_length(input_data):
+# sort the entire corpus based on input length (to minimize padding)
+def sort_input_by_length(dataset):
+    input_data = dataset['en']
     lengths = [(idx, len(line.strip().split())) for idx, line in enumerate(input_data)]
     sorted_lengths = sorted(lengths, key=lambda x: x[1], reverse=True)
-    sorted_input_data = []
-    for idx,_ in sorted_lengths:
-        sorted_input_data.append(input_data[idx])
-    return sorted_input_data
+    sorted_dataset = {'en':[], 'hi':[]}
+    for idx,_ in lengths:
+        sorted_dataset['en'].append(dataset['en'][idx])
+        sorted_dataset['hi'].append(dataset['hi'][idx])
+    return sorted_dataset
 
 
 def get_eng_hi_dataset():
     dataset = load_dataset("cfilt/iitb-english-hindi")
-    input_data = dataset['en']
-    sorted_input_data = sort_input_by_length(input_data)
+    sorted_input_data = sort_input_by_length(dataset)
     return sorted_input_data
 
 
+# pad input ids to make sentence length equal in a batch, make corresponding attention masks
 def pad(input_ids):
     max_length = 0
     for x in input_ids:
@@ -34,4 +37,11 @@ def pad(input_ids):
                 input_ids[i].append(0)
                 mask.append(0)
     
+    return input_ids, input_masks
+
+
+def tokenize(sents):
+    tokenizer = MT5Tokenizer.from_pretrained("THUMT/mGPT")
+    input_ids = tokenizer(sents)['input_ids']
+    input_ids, input_masks = pad(input_ids)
     return input_ids, input_masks
