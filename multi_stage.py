21dda5d23d9f008d5eeaed000e683cd1b78a40d6
Tejomay128 <22d0367@iitb.ac.in>
2023-03-23 06:18:52 -0700

    decode done

--- multi_stage.py
+++ multi_stage.py
@@ -47,7 +47,8 @@ class LLM(nn.Module):
     def encode(self, input_ids, input_mask):
         batch_size = input_ids.shape[0]
         len_prefix = self.params.len_prefix
-        prefix_mask = torch.ones([batch_size, len_prefix])
+        len_sent = input_ids.shape[1]
+        prefix_mask = torch.ones([batch_size, len_prefix], device=input_mask.device)
         attn_mask = torch.cat([prefix_mask, input_mask], dim=1)
 
         keys, values = self.prefix(batch_size)
@@ -71,8 +72,26 @@ class LLM(nn.Module):
             layer_prefix_list.append((k, v))
         
         outputs = self._model(input_ids, past_key_values=layer_prefix_list, attention_mask=attn_mask, use_cache=True)
+        layer_prefix_list = []
 
-        return outputs
+        for i, (key, value) in outputs.past_key_values:
+            k = torch.cat([keys[:,2,i,:,:,:].permute(0,2,1,3), key[:,:,len_prefix + len_sent:,:]], dim=2)
+            v = torch.cat([values[:,2,i,:,:,:].permute(0,2,1,3), value[:,:,len_prefix + len_sent:,:]], dim=2)
+            layer_prefix_list.append((k, v))
+
+        return layer_prefix_list
     
-    def decode(self, input_ids, input_mask, target_mask, mode='train'):
+    def decode(self, target_ids, input_mask, target_mask, past_key_values, mode='train'):
+        batch_size = target_ids.shape[0]
+        len_prefix = self.params.len_prefix
+        prefix_mask = torch.ones([batch_size, len_prefix], device=input_mask.device)
+
+        attn_mask = torch.cat([prefix_mask, input_mask, target_mask], dim=1)
+
+        outputs = self._model(target_ids, past_key_values=past_key_values, attention_mask=attn_mask, use_cache=True)
+        return outputs.logits
+    
+    def forward(self, input_ids, input_mask, target_ids, target_mask):
+        past_key_values = self.encode(input_ids, input_mask)
+        logits = self.decode(target_ids, input_mask, target_mask, past_key_values)
         
