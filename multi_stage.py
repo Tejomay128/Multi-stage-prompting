27fa6d40afb055899307cc91b58b136b1db47394
Tejomay Padole <tejomaypadole128@gmail.com>
2023-03-28 17:54:53 +0530

    new changes

--- multi_stage.py
+++ multi_stage.py
@@ -19,11 +19,13 @@ class Prefix(nn.Module):
             nn.init.xavier_uniform_(self.prefixes)
         
     def forward(self, batch_size):
-        interm = torch.maximum(torch.ones(1), self.reparams)
+        interm = torch.maximum(torch.ones(1, device=self.reparams.device), self.reparams)
+        interm = interm.view([1, 1, interm.shape[0], 1])
+        # print(interm.shape, self.prefixes[:, 0::2, :, :].shape)
         keys = (self.prefixes[:, 0::2, :, :] * interm).repeat([batch_size, 1, 1, 1, 1])
         values = (self.prefixes[:, 1::2, :, :] * interm).repeat([batch_size, 1, 1, 1, 1])
-        keys = keys.view(-1, -1, -1, -1, self.n_heads, self.head_size)
-        values = values.view(-1, -1, -1, -1, self.n_heads, self.head_size)
+        keys = keys.view(keys.shape[0], keys.shape[1], keys.shape[2], keys.shape[3], self.n_heads, self.head_size)
+        values = values.view(values.shape[0], values.shape[1], values.shape[2], values.shape[3], self.n_heads, self.head_size)
 
         # keys/values: (batch_size, n_prefixes ,n_layers, len_prefix, n_heads, head_size)
 
@@ -40,16 +42,21 @@ class LLM(nn.Module):
         self.n_heads = model.config.num_attention_heads
         self.head_size = self.embed_size // self.n_heads
         self.len_prefix = len_prefix
+
         self.prefix = Prefix(model, 3, len_prefix)
         self.logSoftmax = nn.LogSoftmax(dim=2)
+        self.logSoftmax_1 = nn.LogSoftmax(dim=1)
         self.nll = nn.NLLLoss()
-    
+
+
     def encode(self, input_ids, input_mask):
         batch_size = input_ids.shape[0]
         len_prefix = self.len_prefix
         len_sent = input_ids.shape[1]
         prefix_mask = torch.ones([batch_size, len_prefix], device=input_mask.device)
         attn_mask = torch.cat([prefix_mask, input_mask], dim=1)
+        pos_ids = torch.arange(0, input_ids.shape[-1], dtype=torch.long, device=input_mask.device)
+        pos_ids = pos_ids.unsqueeze(0).view(-1, input_ids.shape[-1])
 
         keys, values = self.prefix(batch_size)
         # batch_size, n_prefixes, n_layers, len_prefix, n_heads, head_size => batch_size, n_heads, len_prefix, head_size
@@ -59,11 +66,17 @@ class LLM(nn.Module):
             tup = (keys[:,0,i,:,:,:].permute(0,2,1,3), values[:,0,i,:,:,:].permute(0,2,1,3))
             layer_prefix_list.append(tup)
 
-        outputs = self._model(input_ids, past_key_values=layer_prefix_list, attention_mask=attn_mask, use_cache=True)
+        outputs = self._model(input_ids, 
+                              past_key_values=layer_prefix_list, 
+                              attention_mask=attn_mask, 
+                              position_ids=pos_ids, 
+                              use_cache=True)
 
         #RE-ENCODING
         layer_prefix_list = []
         attn_mask = torch.cat([prefix_mask, input_mask, input_mask], dim=1)
+        pos_ids = torch.arange(0, input_ids.shape[-1], dtype=torch.long, device=input_mask.device)
+        pos_ids = pos_ids.unsqueeze(0).view(-1, input_ids.shape[-1])
 
         #prepend re-encoding prefixes and exclude encoder prefixes in the re-encoding stage
         for i, (key, value) in enumerate(outputs.past_key_values):
@@ -71,11 +84,15 @@ class LLM(nn.Module):
             v = torch.cat([values[:,1,i,:,:,:].permute(0,2,1,3), value[:,:,len_prefix:,:]], dim=2)
             layer_prefix_list.append((k, v))
         
-        outputs = self._model(input_ids, past_key_values=layer_prefix_list, attention_mask=attn_mask, use_cache=True)
+        outputs = self._model(input_ids, 
+                              past_key_values=layer_prefix_list, 
+                              attention_mask=attn_mask, 
+                              position_ids=pos_ids, 
+                              use_cache=True)
         layer_prefix_list = []
 
         #prepare past key values for decoding stage
-        for i, (key, value) in outputs.past_key_values:
+        for i, (key, value) in enumerate(outputs.past_key_values):
             k = torch.cat([keys[:,2,i,:,:,:].permute(0,2,1,3), key[:,:,len_prefix + len_sent:,:]], dim=2)
             v = torch.cat([values[:,2,i,:,:,:].permute(0,2,1,3), value[:,:,len_prefix + len_sent:,:]], dim=2)
             layer_prefix_list.append((k, v))
@@ -89,23 +106,83 @@ class LLM(nn.Module):
 
         #only attend to decode stage prefixes and re-encoding stage hidden states
         attn_mask = torch.cat([prefix_mask, input_mask, target_mask], dim=1)
+        pos_ids = torch.arange(0, target_ids.shape[-1], dtype=torch.long, device=input_mask.device)
+        pos_ids = pos_ids.unsqueeze(0).view(-1, target_ids.shape[-1])
 
-        outputs = self._model(target_ids, past_key_values=past_key_values, attention_mask=attn_mask, use_cache=True)
-        return outputs.logits
+        outputs = self._model(target_ids, 
+                              past_key_values=past_key_values, 
+                              attention_mask=attn_mask, 
+                              position_ids=pos_ids, 
+                              use_cache=True)
+        return outputs.logits, outputs.past_key_values
     
     def forward(self, input_ids, input_mask, target_ids, target_mask):
         past_key_values = self.encode(input_ids, input_mask)
-        logits = self.decode(target_ids, input_mask, target_mask, past_key_values)
+        labels = target_ids[:, 1:]
+        target_ids = target_ids[:, :-1]
+        target_mask = target_mask[:, :-1]
+        logits,_ = self.decode(target_ids, input_mask, target_mask, past_key_values)
 
         # make batch size and sentence length as one dimension
         logits = self.logSoftmax(logits)
         logits = logits.view([logits.shape[0] * logits.shape[1], -1])
-        target_ids = target_ids.view([target_ids.shape[0] * target_ids.shape[1], -1])
-        loss = self.nll(logits, target_ids)
+        labels = labels.flatten()
+        loss = self.nll(logits, labels)
         return loss
 
-    
-    def translate(self):
-        pass
+    @torch.no_grad()
+    def translate(self, device, tokenizer, input_sent, beam_width = 4, token_limit=1024):
+        tok_output = tokenizer(input_sent)
+        input_ids = tok_output['input_ids']
+        input_mask = tok_output['attention_mask']
+        input_ids = torch.tensor(input_ids, device=device)
+        input_mask = torch.tensor(input_mask, device=device)
+        target_mask = torch.ones([1, 1], device=device)
+        # attn_mask = torch.cat([prefix_mask, input_mask, target_mask], dim=1)
 
+        past_key_values = self.encode(input_ids, input_mask)
+
+        start = '</s>'
+        tgt = torch.tensor(tokenizer(start)['input_ids'], device=device)    
+        logits, past_key_values = self.decode(tgt, input_mask, target_mask, past_key_values)
+        print(logits.shape)
+        logits = self.logSoftmax(logits.unsqueeze(0)).squeeze(0)
+        scores, indices = logits.topk(sorted=True, k=beam_width)
+
+        sent = []
+        for i in range(beam_width):
+            sent.append([[indices[0, i].item()], scores[0, i].item()])
+        # new_sent = []
+
+        def check(sent_list):
+            flag = False
+            for i in range(beam_width):
+                if sent_list[i][0][-1] != 1:
+                    flag = True
+                    break
+            return flag
+        #need 4 different past_key_values
+        def search_step(sent_list, past_key_values):
+            new_sent_list = []
+            for i in range(beam_width):
+                token = sent_list[i][0][-1]
+                if token == 1:
+                    new_sent_list.append(sent_list[i])
+                else: 
+                    tgt = torch.tensor([[token]], device=device)
+                    logits, past_key_values = self.decode(tgt, input_mask, target_mask, past_key_values)
+                    logits = nn.functional.softmax(logits, dim=1)
+                    scores, indices = logits.topk(sorted=True, k=beam_width)
+                    for i in range(beam_width):
+                        new_sent_list.append([sent_list[i][0].append(indices[0, i].item()),
+                                               sent_list[i][1] + scores[0, i].item()])
+            new_sent_list.sort(key=lambda x: -x[1])
+            new_sent_list = [new_sent_list[i] for i in range(beam_width)]
+            return new_sent_list, past_key_values
+
+        while check(sent):
+            sent, past_key_values = search_step(sent, past_key_values)
         
+        final_sent = tokenizer.decode(sent[0][0])
+        return final_sent
+
--- prep_data.py
+++ prep_data.py
@@ -1,46 +1,49 @@
 from datasets import load_dataset
-from transformers import MT5Tokenizer
+from transformers import GPT2Tokenizer
 
 
 # sort the entire corpus based on input length (to minimize padding)
-def sort_input_by_length(dataset):
-    lengths = [(idx, len(line['en'].strip().split())) for idx, line in enumerate(dataset)]
+def sort_input_by_length(dataset, lang1='en'):
+    symbols = ['<', '>']
+    lengths = [(idx, len(line[lang1].strip().split())) for idx, line in enumerate(dataset)]
     sorted_lengths = sorted(lengths, key=lambda x: x[1], reverse=True)
     sorted_dataset = []
     for i, (idx,_) in enumerate(sorted_lengths):
-        # print(f"En: {dataset[idx]['en']} || Hi: {dataset[idx]['hi']}")
-        if dataset[idx]['en'] != '':
+        # print(f"En: {dataset[idx][lang1]} || Hi: {dataset[idx]['hi']}")
+        flag = False
+        for symbol in symbols:
+            if symbol in dataset[idx][lang1]:
+                flag = True
+                break
+        if len(dataset[idx][lang1]) > 0 and not flag:
             sorted_dataset.append(dataset[idx])
     return sorted_dataset
 
+#filters out sentences that won't fit within the model's token limit
+def filter_dataset(tokenizer, token_limit, len_prefix, dataset, lang1='en', lang2='hi'):
+    new_dataset = []
+    for pair in dataset:
+        if len_prefix + len(pair[lang1]) + len(pair[lang2]) < token_limit:
+            tok1 = tokenizer(pair[lang1])['input_ids']
+            tok2 = tokenizer(pair[lang2])['input_ids']
+            if len_prefix + len(tok1) + len(tok2) < token_limit:
+                new_dataset.append(pair)
+    return new_dataset
 
-def get_eng_hi_dataset():
+
+def get_eng_hi_dataset(val_split=0.8):
     dataset = load_dataset("cfilt/iitb-english-hindi")
-    sorted_data = sort_input_by_length(dataset['train']['translation'])
-    return sorted_data, dataset['test']['translation']
+    # train_data = filter_dataset(tokenizer, token_limit, len_prefix, dataset['train']['translation'])
+    # test_data =filter_dataset(tokenizer, token_limit, len_prefix, dataset['test']['translation'])
+    train_data = dataset['train']['translation']
+    test_data = dataset['test']['translation']
 
+    split_idx = int(len(train_data) * val_split)
+    val_data = train_data[split_idx:]
+    train_data = train_data[:split_idx]
 
-# pad input ids to make sentence length equal in a batch, make corresponding attention masks
-def pad(input_ids):
-    max_length = 0
-    for x in input_ids:
-        max_length = max(max_length, len(x))
-    
-    input_masks = []
-    for i, x in enumerate(input_ids):
-        mask = []
-        for i in range(max_length):
-            if i < len(x):
-                mask.append(1)
-            else:
-                input_ids[i].append(0)
-                mask.append(0)
-    
-    return input_ids, input_masks
+    train_data = sort_input_by_length(train_data)
+    val_data = sort_input_by_length(val_data)
+    test_data = sort_input_by_length(test_data)
 
-
-def tokenize(sents):
-    tokenizer = MT5Tokenizer.from_pretrained("THUMT/mGPT")
-    input_ids = tokenizer(sents)['input_ids']
-    input_ids, input_masks = pad(input_ids)
-    return input_ids, input_masks
+    return train_data, val_data, test_data
